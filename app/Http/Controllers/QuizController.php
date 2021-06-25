3028b93e11284390daae6a4b3cae99c8d749a31e
Yuta-Saito0310 <y-saito-3z4@eagle.sophia.ac.jp>
2021-06-25 17:33:22 +0900

    $categoryをするためにddをした

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -22,6 +22,7 @@ class QuizController extends Controller
     {
         $categoryID= $request->id;
         $category = Category::find($categoryID);
+        dd($category);
         $quizzes = DB::table('quizzes')
                     ->select('id','title','image_name','correct','uncorrect1','uncorrect2','explain_sentence')
                     ->where([
